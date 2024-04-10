0b81c0b6917ba1d6beabe353bdb6c91bc40660a2
Rapiwash <sistemarapiwash@gmail.com>
2024-04-10 09:31:54 -0500

    change - time zone

--- src/services/global.js
+++ src/services/global.js
@@ -1,38 +1,40 @@
 // Zona Horaria
-export const timeZone = "America/Lima";
+export const timeZone = "America/Guatemala";
 // Mostrar Opcion de Registrar Ordenes Antiguas
 export const oldOrder = true;
 // Tipo de Moneda
-export const tipoMoneda = "PEN";
+export const tipoMoneda = "GTQ";
 // Formato de la moneda (separacion de miles - decimales) segun la configuracion regional
-export const confMoneda = "es-PE";
-export const simboloMoneda = "S/";
-export const nameMoneda = "Soles";
-export const ingresoDigital = "YAPE";
+export const confMoneda = "es-GT";
+export const simboloMoneda = "Q";
+export const nameMoneda = "Quetzales";
+export const ingresoDigital = "TRANSFERENCIA MOVIL";
 
 export const politicaAbandono = {
   mResaltado: "El plazo maximo para retirar las prendas es de 20 dias ",
   mGeneral: `despues de entregada a la lavanderia; vencido el plazo se donara a instituciones de caridad.No hay lugar a
    reclamo una ves retirada la prenda No nos reposabilizamos por prendas que se destiñan por malos tintes, botones o adornos 
    que no resistan al lavado o planchado, por las prendas que se deterioren por estar demasiado usadas, tejidos y confecciones defectuosas.
-    La indemnización por ropa perdida o malograda se ajusta de acuerdo a la ley R.S. 2322 que equivale al 20% del valor de la prenda. No nos reponsabilizamos por dinero u objetos de valor dejados en la prenda`,
+    La indemnización por ropa perdida o malograda se ajusta de acuerdo a la ley de Profeco que equivale a 3 veces del valor de la nota total de venta.`,
 };
 
-export const documento = "DNI"; // DNI
+export const documento = "DPI / Celular"; // DNI
 
-export const nameImpuesto = "IGV"; // IGV
+export const nameImpuesto = "IVA"; // IGV
 
-export const codigoPhonePais = "51";
+export const codigoPhonePais = "502";
 
 export const MONTOS_BASE = [
+  { monto: 200, cantidad: "", total: 0 },
   { monto: 100, cantidad: "", total: 0 },
   { monto: 50, cantidad: "", total: 0 },
   { monto: 20, cantidad: "", total: 0 },
   { monto: 10, cantidad: "", total: 0 },
   { monto: 5, cantidad: "", total: 0 },
-  { monto: 2, cantidad: "", total: 0 },
   { monto: 1, cantidad: "", total: 0 },
   { monto: 0.5, cantidad: "", total: 0 },
-  { monto: 0.2, cantidad: "", total: 0 },
+  { monto: 0.25, cantidad: "", total: 0 },
   { monto: 0.1, cantidad: "", total: 0 },
+  { monto: 0.05, cantidad: "", total: 0 },
+  { monto: 0.01, cantidad: "", total: 0 },
 ];
