9c0e4513636fdca7dd97dfbf9b11c8fbcaea3898
Rapiwash <sistemarapiwash@gmail.com>
2024-04-11 10:24:40 -0500

    change de nota

--- src/services/global.js
+++ src/services/global.js
@@ -11,8 +11,9 @@ export const nameMoneda = "Quetzales";
 export const ingresoDigital = "TRANSFERENCIA MOVIL";
 
 export const politicaAbandono = {
-  mResaltado: "El plazo maximo para retirar las prendas es de 20 dias ",
-  mGeneral: `despues de entregada a la lavanderia; vencido el plazo se donara a instituciones de caridad.No hay lugar a
+  mResaltado:
+    "El plazo maximo para retirar es de 8 días, de lo contrario no nos responsabilizamos de las mismas",
+  mGeneral: `; vencido el plazo se donara a instituciones de caridad.No hay lugar a
    reclamo una ves retirada la prenda No nos reposabilizamos por prendas que se destiñan por malos tintes, botones o adornos 
    que no resistan al lavado o planchado, por las prendas que se deterioren por estar demasiado usadas, tejidos y confecciones defectuosas.`,
 };
